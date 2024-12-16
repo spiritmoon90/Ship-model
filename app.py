9eaca621817dfecf393168fcc53f22a6a6807883
spiritmoon90 <113458976+spiritmoon90@users.noreply.github.com>
2024-12-15 23:06:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='The Ship Crew number is  $ {}'.format(output))
+    return render_template('index.html', prediction_text='The Ship Crew number is  {}'.format(output))
 
 
 if __name__ == "__main__":
